cd70a4445599a7a1b4c2ea6461806a598858a92b
xubiao09 <30366645+xubiao09@users.noreply.github.com>
2017-07-23 14:20:53 +0800

    Update PlotTrajct.m

--- PlotTrajct.m
+++ PlotTrajct.m
@@ -1,13 +1,17 @@
-function PlotTrajct(green,v,d)
+function PlotTrajct(green,v0,v,d)
 %%
-%»­Í¼£¬greenÎªÂÌµÆÏàÎ»£¬vÎª¸÷¶Î³µËÙ£¬dÎª¾àÀë
+%ç”»å›¾ï¼Œgreenä¸ºç»¿ç¯ç›¸ä½ï¼Œvä¸ºå„æ®µè½¦é€Ÿï¼Œdä¸ºè·ç¦»
 %%
 d=d(:);
 v=v(:);
-NumIntsct=length(v);  %½»²æ¿ÚÊıÄ¿
+NumIntsct=length(v);  %äº¤å‰å£æ•°ç›®
 SumD=cumsum(d);
-t=d./v;
-SumT=cumsum(t);
+t0=0;
+d0=0;
+t_plot=[];
+d_plot=[];
+v_plot=[];
+figure(2);
 hold on;
 for i=1:NumIntsct
     G=green{i};
@@ -15,11 +19,41 @@ for i=1:NumIntsct
         plot(G(:,j),[SumD(i);SumD(i)],'g','LineWidth',2);
     end
     if(i~=1)
-        plot([SumT(i-1),SumT(i)],[SumD(i-1),SumD(i)],'y','LineWidth',2);
+        %plot([SumT(i-1),SumT(i)],[SumD(i-1),SumD(i)],'y','LineWidth',2);
+        t1=t0+abs(v(i)-v(i-1))/1.5;
+        d1=SumD(i-1)+abs(v(i)^2-v(i-1)^2)/2/1.5;
+        t_acce=linspace(t0,t1,10);
+        d_acce=SumD(i-1)+v(i-1)*(t_acce-t0)+0.5*1.5*(t_acce-t0).^2;
+        v_acce=linspace(v(i-1),v(i),10);
+        t2=t1+(SumD(i)-d1)/v(i);
+        t_plot=[t_plot,t_acce,t2];
+        d_plot=[d_plot,d_acce,SumD(i)];
+        v_plot=[v_plot,v_acce,v(i)];
+        t0=t2;
     else
-        plot([0,SumT(i)],[0,SumD(i)],'y','LineWidth',2);
+        %plot([0,SumT(i)],[0,SumD(i)],'y','LineWidth',2);
+        t1=t0+abs(v(i)-v0)/1.5;
+        d1=0+abs(v(i)^2-v0^2)/2/1.5;
+        t_acce=linspace(t0,t1,10);
+        d_acce=0+v0*(t_acce-t0)+0.5*1.5*(t_acce-t0).^2;
+        v_acce=linspace(v0,v(i),10);
+        t2=t1+(SumD(i)-d1)/v(i);
+        t_plot=[t_plot,t_acce,t2];
+        d_plot=[d_plot,d_acce,SumD(i)];
+        v_plot=[v_plot,v_acce,v(i)];
+        t0=t2;
     end
 end
+plot(t_plot,d_plot,'LineWidth',2);
 xlabel('t[s]');
 ylabel('d[m]');
-xlim([0,max(SumT)+10]);
+xlim([0,max(t_plot)+10]);
+hold off;
+figure(3);
+hold on;
+plot(t_plot,v_plot,'LineWidth',2);
+xlabel('t[s]');
+ylabel('v[m/s]');
+ylim([0,20]);
+grid on;
+hold off;
