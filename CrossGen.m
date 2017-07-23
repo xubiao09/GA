f2cad7d0673d6532e1af12fecacc48e8dafb30f2
Yang Zheng <zhengy093@gmail.com>
2017-07-23 15:23:15 +0100

    Improve speed

--- CrossGen.m
+++ CrossGen.m
@@ -2,5 +2,6 @@ function [v3,v4] = CrossGen(v1,v2,alpha)
 %%
 %线性交叉算子，交叉系数为alpha,母代为v1和v2，子代为v3和v4
 %%
- v3 = alpha*v1+(1-alpha)*v2;
- v4 = (1-alpha)*v1+alpha*v2;
+alpha = 2*rand(1);
+ v3 = alpha*v1 + (1-alpha)*v2;
+ v4 = (1-alpha)*v1 + alpha*v2;
